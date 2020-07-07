087a65ca5df217a72ab532fb02bdcd1629f2ff4c
lesouq <53365054+lesouq@users.noreply.github.com>
2020-07-07 16:15:44 +0200

    Add files via upload

---
+++ hello-web-client-service/src/test/java/com/microservice/hellowebclientservice/HelloWebClientServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservice.hellowebclientservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloWebClientServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
